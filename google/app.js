cb919f8f7024887678ce986df5dca7de46e1055a
Rafal Nojek <rafalnojek@student.agh.edu.pl>
2021-06-07 00:29:22 +0200

    ukonczone zajecia 7

--- google/app.js
+++ google/app.js
@@ -1,3 +1,6 @@
+const shared = {
+    googleSearch:null
+  }
 
     var app = new Vue({
         el: '#app',
@@ -15,25 +18,53 @@
           
         },
         computed:{
-            searchCities(){
-
-                let result = this.googleSearch.toLowerCase();
-                if(result.length >3){
-                    return this.cities.filter((cityData) => {
-                        return cityData.nameLowerCase.includes(result);
-                    }).slice(0, 9);
+            searchCities() {
+                if (this.googleSearch.length === 0) {
+                    return [];
                 }
+                let returnedCities = [];
+                let searchLowerCase = this.googleSearch.toLowerCase();
 
-                      
+                this.cities.forEach((cityData) => {
+                    if (returnedCities.length === 10 || !cityData.nameLowerCase.includes(searchLowerCase)) {
+                        return;
+                    }
+                    returnedCities.push({
+                        name: cityData.name,
+                        nameHtml: cityData.nameLowerCase.replace(searchLowerCase, (match) => {
+                            return '<b>' + match + '</b>';
+                            //match.charAt(0).toUpperCase() + match.slice(1)
+                        })
+                    })
+                });
                 
-                
-                
+                return returnedCities;
             }
+            
+        },
+        methods: {
+            changeResults(){
+                document.body.classList.add("results");
+            },
+            sendMessage(){
+                app.$emit('send-message', googleSearch)
+              }
 
-        }
+        },
         
-    })
+    });
+    
 
+
+    var app2 = new Vue({
+        el:"#app-two",
+        data:{
+          shared
+        },
+        mounted(){
+            app.$on("send-message", message => this.message = message);
+        }
+      })
 // var changer = '';
 // function addresults(){
 
@@ -44,27 +75,3 @@
     
 // }
 
-// var app2 = new Vue({
-//     el:'#app2',
-//     data:{
-//         googleSearch: "",
-//         cities: window.cities,  
-//         filteredCities:[]
-//     }
-// ,
-//     computed: {
-//         searchCities: function() {
-//             if(this.googleSearch.length >2){
-//                 var filtered = cities.filter(function (el) {
-//                     return (el => el.name.startsWith(this.googleSearch));
-//                   });
-                  
-//                 filtered = filtered.slice(0,10);
-//                 filtered.forEach(element => this.filteredCities.unshift(element))
-
-                
-               
-//             }  
-//         }   
-//     }
-// })
