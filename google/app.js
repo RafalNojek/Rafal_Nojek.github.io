630abc11b76a19381ca6947cfdc7eda9438d6e54
Rafal Nojek <rafalnojek@student.agh.edu.pl>
2021-06-06 22:52:07 +0200

    zadanie z zajec 6 wykonane

--- google/app.js
+++ google/app.js
@@ -3,49 +3,68 @@
         el: '#app',
         data: {
             googleSearch: '',
-            cities: window.cities,
+            cities: window.cities.map((cityData) => {
+                cityData.nameLowerCase = cityData.name.toLowerCase();
+                return cityData;}),
             filteredCities: [],
         },
         updated:function() {
-            if(app.googleSearch !== '')
+            if(app.googleSearch.length >=10)
                 document.body.classList.add("results");
                 console.log(app.googleSearch)
           
         },
+        computed:{
+            searchCities(){
+
+                let result = this.googleSearch.toLowerCase();
+                if(result.length >3){
+                    return this.cities.filter((cityData) => {
+                        return cityData.nameLowerCase.includes(result);
+                    }).slice(0, 9);
+                }
+
+                      
+                
+                
+                
+            }
+
+        }
         
     })
 
-var changer = '';
-function addresults(){
+// var changer = '';
+// function addresults(){
 
-    changer = document.getElementById("changer");
-   if(changer!==''){
-    document.body.classList.add("results");
-   } 
+//     changer = document.getElementById("changer");
+//    if(changer!==''){
+//     document.body.classList.add("results");
+//    } 
     
-}
+// }
 
-var app2 = new Vue({
-    el:'#app2',
-    data:{
-        googleSearch: "",
-        cities: window.cities,  
-        filteredCities:[]
-    }
-,
-    computed: {
-        searchCities: function() {
-            if(this.googleSearch.length >2){
-                var filtered = cities.filter(function (el) {
-                    return (el => el.name.startsWith(this.googleSearch));
-                  });
+// var app2 = new Vue({
+//     el:'#app2',
+//     data:{
+//         googleSearch: "",
+//         cities: window.cities,  
+//         filteredCities:[]
+//     }
+// ,
+//     computed: {
+//         searchCities: function() {
+//             if(this.googleSearch.length >2){
+//                 var filtered = cities.filter(function (el) {
+//                     return (el => el.name.startsWith(this.googleSearch));
+//                   });
                   
-                filtered = filtered.slice(0,10);
-                filtered.forEach(element => this.filteredCities.unshift(element))
+//                 filtered = filtered.slice(0,10);
+//                 filtered.forEach(element => this.filteredCities.unshift(element))
 
                 
                
-            }  
-        }   
-    }
-})
+//             }  
+//         }   
+//     }
+// })
